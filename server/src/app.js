59c33bc87472b15b7a35dcdd59217d555ec06de5
phuocnnt464 <96932969+phuocnnt464@users.noreply.github.com>
2025-11-18 14:24:46 +0700

    add header allow to cache-control

--- server/src/app.js
+++ server/src/app.js
@@ -52,7 +52,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control'],
   exposedHeaders: ['X-Total-Count', 'X-Page', 'X-Per-Page']
 }));
 
