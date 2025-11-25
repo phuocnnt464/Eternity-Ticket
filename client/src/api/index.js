82c35265e10b5c26266312a1aba4ce3b082a3b1e
phuocnnt464 <96932969+phuocnnt464@users.noreply.github.com>
2025-11-25 17:02:40 +0700

    fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,19 +11,3 @@ export { queueAPI } from './queue'
 export { notificationsAPI } from './notifications'
 export { refundAPI } from './refund'
 export { couponAPI } from './coupon'  
-
-// Default export for convenience
-export default {
-  auth: require('./auth').authAPI,
-  events: require('./events').eventsAPI,
-  orders: require('./orders').ordersAPI,
-  users: require('./users').usersAPI,
-  admin: require('./admin').adminAPI,
-  membership: require('./membership').membershipAPI,
-  sessions: require('./sessions').sessionsAPI,
-  checkin: require('./checkin').checkinAPI,
-  queue: require('./queue').queueAPI,
-  notifications: require('./notifications').notificationsAPI,
-  refund: require('./refund').refundAPI,
-  coupon: require('./coupon').couponAPI
-}
