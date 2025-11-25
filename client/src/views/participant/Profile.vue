3688d70d43c5362ac575004e037413351ca62d96
phuocnnt464 <96932969+phuocnnt464@users.noreply.github.com>
2025-11-26 03:14:44 +0700

    fix profile

--- client/src/views/participant/Profile.vue
+++ client/src/views/participant/Profile.vue
@@ -81,7 +81,7 @@ const loadProfile = async () => {
         avatarPreview.value = user.avatar_url
       } else {
         // Prepend API base URL
-        const baseUrl =  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
+        const baseUrl = 'http://localhost:3000'
         avatarPreview.value = `${baseUrl}${user.avatar_url}` 
       }
     } else {
